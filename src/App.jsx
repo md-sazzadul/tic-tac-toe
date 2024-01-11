5f17fba265d13ddec0e78c6a2497aa770f85a958
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-01-11 11:25:25 +0100

    added square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Square from "./components/Square/Square";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello Tic Tac Toe</h1>;
+  return <Square></Square>;
 }
---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,9 @@
+const Square = () => {
+  return (
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+      X
+    </button>
+  );
+};
+
+export default Square;
