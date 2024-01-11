2c5f0887888d368445b83431fc037d25e8c8432d
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-01-11 11:43:09 +0100

    added value as props in square component

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,7 +1,7 @@
-const Square = () => {
+const Square = ({ value }) => {
   return (
     <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-      1
+      {value}
     </button>
   );
 };
