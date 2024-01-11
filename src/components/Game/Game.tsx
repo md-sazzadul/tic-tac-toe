143de10fbe64b4af3da4ccb020fd63304b7eb4bb
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-01-11 16:23:36 +0100

    added history

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -4,13 +4,35 @@ import Board from "../Board/Board";
 const Game = () => {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     setXIsNext(!xIsNext);
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to the move # ${move}`;
+    } else {
+      description = `Go to start the game`;
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div>
@@ -22,7 +44,7 @@ const Game = () => {
         ></Board>
       </div>
       <div>
-        <ol></ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
