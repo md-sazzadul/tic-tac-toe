cdc9fa412d8eebd53dc785fe070a89675224efdb
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-01-11 13:35:25 +0100

    added toggler state to control move

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -3,29 +3,58 @@ import Square from "../Square/Square";
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
-  function handleClick() {
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[0] = "X";
+    xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
     <>
       <div className="flex">
-        <Square value={squares[0]} onSquareClick={handleClick}></Square>
-        <Square value={squares[1]} onSquareClick={handleClick}></Square>
-        <Square value={squares[2]} onSquareClick={handleClick}></Square>
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+        ></Square>
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+        ></Square>
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+        ></Square>
       </div>
       <div className="flex">
-        <Square value={squares[3]} onSquareClick={handleClick}></Square>
-        <Square value={squares[4]} onSquareClick={handleClick}></Square>
-        <Square value={squares[5]} onSquareClick={handleClick}></Square>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+        ></Square>
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+        ></Square>
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+        ></Square>
       </div>
       <div className="flex">
-        <Square value={squares[6]} onSquareClick={handleClick}></Square>
-        <Square value={squares[7]} onSquareClick={handleClick}></Square>
-        <Square value={squares[8]} onSquareClick={handleClick}></Square>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+        ></Square>
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+        ></Square>
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+        ></Square>
       </div>
     </>
   );
