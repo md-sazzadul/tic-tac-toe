13db6ce397a27c1abc4aefbb8b58bb54adb00a78
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-01-11 13:51:10 +0100

    added function to calculate result

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -5,6 +5,15 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = "Next Player " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i) {
     if (squares[i]) {
       return;
@@ -15,8 +24,33 @@ const Board = () => {
     setXIsNext(!xIsNext);
   }
 
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   return (
     <>
+      <div>{status}</div>
       <div className="flex">
         <Square
           value={squares[0]}
