4fb8d4b01e5461b821db331c93c0847ca3e39f38
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-12-10 11:18:42 +0600

    added winning line highlight

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -3,7 +3,7 @@ const Square = ({ value, onSquareClick, isWinningSquare }) => {
     <button
       className={`h-12 w-12 m-1 leading-9 text-lg font-bold border border-gray-400 rounded-md shadow-md transition duration-300 ${
         isWinningSquare
-          ? "bg-yellow-300 text-gray-800"
+          ? "winning-line"
           : "bg-purple-500 text-gray-800 hover:bg-gray-200"
       } `}
       onClick={onSquareClick}
