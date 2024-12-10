3a12ad536d2796cbe12487813c6bfd6049262050
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-12-10 11:08:17 +0600

    improved UX with sounds

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,6 +1,9 @@
 import { calculateWinner } from "../../utils";
 import Square from "../Square/Square";
 
+const clickSound = new Audio("/public/sounds/click.wav");
+const winSound = new Audio("/public/sounds/win.mp3");
+
 const Board = ({ xIsNext, squares, onPlay, playerX, playerO }) => {
   const winnerInfo = calculateWinner(squares);
   const winner = winnerInfo ? winnerInfo.winner : null;
@@ -9,6 +12,7 @@ const Board = ({ xIsNext, squares, onPlay, playerX, playerO }) => {
 
   if (winner) {
     status = `Winner: ${winner === "X" ? playerX : playerO}`;
+    winSound.play();
   } else {
     status = "Next Player " + (xIsNext ? playerX : playerO);
   }
@@ -17,6 +21,7 @@ const Board = ({ xIsNext, squares, onPlay, playerX, playerO }) => {
     if (squares[i] || winner) {
       return;
     }
+    clickSound.play();
     const nextSquares = squares.slice();
     xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
     onPlay(nextSquares);
