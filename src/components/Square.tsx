416216aeccaa999c016520f6cdd9ac953ec93c3e
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-12-31 16:06:48 +0600

    lift state up

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
+interface SquareProps {
+  value: string | null;
+  onSquareClick: () => void;
+}
 
-export default function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+export default function Square({ value, onSquareClick }: SquareProps) {
   return (
     <button
       className="w-16 h-16 bg-blue-500 text-white text-2xl font-bold rounded shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
